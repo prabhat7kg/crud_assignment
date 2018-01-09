84642ec42090caa131db4eaf766d081e4f14c50a
prabhat <prabhat@excellencetechnologies.in>
2018-01-09 18:19:16 +0530

    undo commit

--- Model/ResourceModel/Demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Assignment\Crud\Model\ResourceModel;
-class Demo extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
-{
-    protected function _construct()
-    {
-        $this->_init('assignment_crud_demo','assignment_crud_demo_id');
-    }
-}
--- Model/ResourceModel/Demo/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Assignment\Crud\Model\ResourceModel\Demo;
-class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
-{
-    protected function _construct()
-    {
-        $this->_init('Assignment\Crud\Model\Demo','Assignment\Crud\Model\ResourceModel\Demo');
-    }
-}
